c91d1ddd294c492ddda614c65f3960d1421f24bc
Dave Nicolette <davenicolette@gmail.com>
2016-11-22 13:19:54 -0700

    Made Application extend SpringBootServletInitializer

--- src/main/java/com/neopragma/javahelloservice/Application.java
+++ src/main/java/com/neopragma/javahelloservice/Application.java
@@ -2,11 +2,12 @@ package com.neopragma.javahelloservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
  
 @ComponentScan
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
  
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
